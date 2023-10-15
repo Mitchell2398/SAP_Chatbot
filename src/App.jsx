91861753826d8557a4feffe9cb1e4144227d2127
Neil Stuart <neil.stuart11@gmail.com>
2023-10-15 16:13:10 +0100

    Add prompts file for prompts, refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { TicketTable } from "./components/TicketTable/TicketTable";
 import Chatbot from "./components/Chatbot/Chatbot";
+import { useState } from "react";
 
 function App() {
+  const [ticket, setTicket] = useState({
+    subject: "TBD",
+    priority: "TBD",
+    category: "TBD",
+    description: "TBD",
+  });
+
   return (
     <div className="flex flex-col sm:flex-row p-5 bg-slate-200 w-screen gap-5 h-screen items-center max-h-screen">
-      <Chatbot />
-      <TicketTable />
+      <Chatbot setTicket={setTicket} ticket={ticket} />
+      <TicketTable subject={ticket.subject} priority={ticket.priority} category={ticket.category} description={ticket.description} />
 
     </div>
   );
