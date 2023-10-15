7cc3ec0e21e6e4b1aab18abaee55488c3048704e
Neil Stuart <neil.stuart11@gmail.com>
2023-10-15 16:04:37 +0100

    Fix ticket table

--- src/components/TicketTable/TicketTable.jsx
+++ src/components/TicketTable/TicketTable.jsx
@@ -1,4 +1,4 @@
-export const TicketTable = ({ ticket }) => {
+export const TicketTable = ({ subject, priority, category, description }) => {
   return (
     <div className="sm:w-[50%] h-[80%] w-full flex flex-col bg-slate-950 p-5 rounded-xl">
       <div className="flex flex-row justify-between items-center">
@@ -9,21 +9,21 @@ export const TicketTable = ({ ticket }) => {
         <div className="flex flex-row justify-between gap-5 mx-3">
           <div className="flex border-slate-600 flex-row gap-5 items-center gap-5 justify-start">
             <h1 className="text-lg">Subject</h1>
-            <p className="text-slate-300">{ticket.subject ? ticket.subject : "N/A"}</p>
+            <p className="text-slate-300">{subject ? subject : "N/A"}</p>
           </div>
           <div className="flex flex-row gap-3 items-center  p-2 rounded-lg border-2 border-emerald-700 justify-start">
             <h1 className="text-base font-semibold ">Priority</h1>
-            <p className="text-base text-slate-300">{ticket.priority ? ticket.priority : "N/A"}</p>
+            <p className="text-base text-slate-300">{priority ? priority : "N/A"}</p>
           </div>
         </div>
 
         <div className="flex flex-row p-3 justify-start gap-5 items-center">
           <h1 className="text-lg">Category</h1>
-          <p className="text-slate-300">{ticket.category ? ticket.category : "N/A"}</p>
+          <p className="text-slate-300">{category ? category : "N/A"}</p>
         </div>
         <div className="flex p-3 border-t border-slate-600 flex-col">
-          <h1 className="text-lg mb-1">Description</h1>
-          <p className="text-slate-300 pl-3">{ticket.description ? ticket.description : "N/A"}</p>
+          <h1 className="text-lg mb-3">Description</h1>
+          <p className="text-slate-300">{description ? description : "N/A"}</p>
         </div>
       </div>
     </div>
