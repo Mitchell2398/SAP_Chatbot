02333de74c646c5d014408757d668d7ccbeb3c38
Jack Mitchell <jackmitchell2398@gmail.com>
2023-10-20 20:33:44 +0100

    added ability to edit fields

--- src/components/TicketTable/TicketTable.jsx
+++ src/components/TicketTable/TicketTable.jsx
@@ -1,4 +1,20 @@
+import React, { useState } from "react";
+
 export const TicketTable = ({ subject, priority, category, description }) => {
+  // Initialize state for subject, priority, category, and description
+  const [formData, setFormData] = useState({
+    subject: subject ? subject : "",  
+    priority: priority ? priority : "",
+    category: category ? category : "",
+    description: description ? description : "",
+  });
+
+  // Handle changes for all fields
+  const handleFieldChange = (event, fieldName) => {
+    const value = event.target.value;
+    setFormData({ ...formData, [fieldName]: value });
+  };
+
   return (
     <div className="sm:w-[50%] h-[80%] w-full flex flex-col bg-slate-950 p-5 rounded-xl">
       <div className="flex flex-row justify-between items-center">
@@ -9,21 +25,40 @@ export const TicketTable = ({ subject, priority, category, description }) => {
         <div className="flex flex-row justify-between gap-5 mx-3">
           <div className="flex border-slate-600 flex-row gap-5 items-center gap-5 justify-start">
             <h1 className="text-lg">Subject</h1>
-            <p className="text-slate-300">{subject ? subject : "N/A"}</p>
+            <input
+              type="text"
+              className="text-slate-300"
+              value={formData.subject}
+              onChange={(event) => handleFieldChange(event, "subject")}
+            />
           </div>
-          <div className="flex flex-row gap-3 items-center  p-2 rounded-lg border-2 border-emerald-700 justify-start">
-            <h1 className="text-base font-semibold ">Priority</h1>
-            <p className="text-base text-slate-300">{priority ? priority : "N/A"}</p>
+          <div className="flex flex-row gap-3 items-center p-2 rounded-lg border-2 border-emerald-700 justify-start">
+            <h1 className="text-base font-semibold">Priority</h1>
+            <input
+              type="text"
+              className="text-base text-slate-300"
+              value={formData.priority}
+              onChange={(event) => handleFieldChange(event, "priority")}
+            />
           </div>
         </div>
 
         <div className="flex flex-row p-3 justify-start gap-5 items-center">
           <h1 className="text-lg">Category</h1>
-          <p className="text-slate-300">{category ? category : "N/A"}</p>
+          <input
+            type="text"
+            className="text-slate-300"
+            value={formData.category}
+            onChange={(event) => handleFieldChange(event, "category")}
+          />
         </div>
         <div className="flex p-3 border-t border-slate-600 flex-col">
           <h1 className="text-lg mb-3">Description</h1>
-          <p className="text-slate-300">{description ? description : "N/A"}</p>
+          <textarea
+            className="text-slate-300"
+            value={formData.description}
+            onChange={(event) => handleFieldChange(event, "description")}
+          />
         </div>
       </div>
     </div>
