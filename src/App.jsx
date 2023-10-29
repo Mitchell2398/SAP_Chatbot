e20cdb789240152da3fda1b036c3a093c5200e8f
Neil Stuart <neil.stuart11@gmail.com>
2023-10-29 13:24:47 +0000

    Add bg-image

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,22 @@ function App() {
     completed: false,
     editable: false,
     submitted: false,
+
+
   });
 
+  const bgImage = {
+    backgroundImage: `url("/bg-image2.png")`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    height: "100vh",
+    width: "100vw",
+  }
   return (
-    <div className="flex flex-col lg:flex-row p-5 bg-slate-200 w-screen gap-5 h-screen items-center max-h-screen">
+    <div style={bgImage} className="flex flex-col lg:flex-row p-5 splash-bg bg-slate-200 w-screen gap-5 h-screen items-center max-h-screen">
       <Chatbot setTicket={setTicket} ticket={ticket} />
       <TicketTable ticket={ticket} setTicket={setTicket}/>
-
     </div>
   );
 }
