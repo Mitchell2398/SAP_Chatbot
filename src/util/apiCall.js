faccd3c2c107f66a8c2762fd41fb0be790d9f860
Neil Stuart <neil.stuart11@gmail.com>
2023-11-02 12:31:29 +0000

    Add retries to API calls

--- src/util/apiCall.js
+++ src/util/apiCall.js
@@ -5,9 +5,62 @@ const openai = new OpenAI({
   dangerouslyAllowBrowser: true,
 });
 
+const maxRetries = 10;
+const retryDelay = 4500;
+
+const retryAsyncFunc = async (attempt) => {
+  let retries = 0;
+
+  while (retries < maxRetries) {
+    try {
+      // Current timestamp ms
+      const timeMs = new Date().getTime();
+      console.log(
+        "OpenAPI Request: attempt " +
+          (retries + 1) +
+          " of " +
+          maxRetries +
+          ". With timeout " +
+          retryDelay +
+          "ms"
+      );
+      const response = await Promise.race([
+        attempt(),
+        new Promise((resolve) =>
+          setTimeout(() => {
+            resolve();
+            retries++;
+          }, retryDelay)
+        ),
+      ]);
+
+      if (response) {
+        console.log(
+          
+          (response === "Success"?"TICKET DATA: ":"CHAT MESSAGE: ") +"OpenAPI Request: Success, Took " +
+            (
+              (new Date().getTime() - timeMs + retryDelay * retries) /
+              1000
+            ).toFixed(2) +
+            "s total and " +
+            (retries +
+            1) +
+            " attempts."
+        );
+        return response;
+      }
+    } catch (error) {
+      console.error(`Attempt ${retries + 1} failed: ${error}`);
+      retries++;
+    }
+  }
+
+  throw new Error("Max retries reached, request failed.");
+};
+
 export const getOpenAICompletion = async (messages) => {
-  try {
-    const response = await openai.chat.completions.create({
+  const attempt = async () => {
+    return openai.chat.completions.create({
       model: "gpt-3.5-turbo-0613",
       messages: messages,
       presence_penalty: -0.3,
@@ -15,25 +68,25 @@ export const getOpenAICompletion = async (messages) => {
       frequency_penalty: -0.3,
       functions: [
         {
-          "name": "nextTask",
-          "description": "Once you get enough information to complete the task",
-          "parameters": {
-            "type": "object",
-            "properties": {},
+          name: "nextTask",
+          description: "Once you get enough information to complete the task",
+          parameters: {
+            type: "object",
+            properties: {},
           },
-          "required" : ["subject", "replication steps", ["permission"]]
-        }
-      ]
-
+          required: ["subject", "replication steps", ["permission"]],
+        },
+      ],
     });
-    return response.choices[0].message;
-  } catch (error) {
-    console.error(error);
-  }
+  };
+
+  const reponse = await retryAsyncFunc(attempt);
+
+  return reponse.choices[0].message;
 };
 
-
 export const completeTicketOpenAi = async (messages, func, ticket, setData) => {
+  const attemps = async () => {
     try {
       const response = await openai.chat.completions.create({
         model: "gpt-3.5-turbo-0613",
@@ -41,78 +94,98 @@ export const completeTicketOpenAi = async (messages, func, ticket, setData) => {
         presence_penalty: -0.3,
         temperature: 0.2,
         frequency_penalty: -0.3,
-        functions: [func]
-  
+        functions: [func],
       });
       if (response.choices[0].message.function_call) {
         const functionArgs = JSON.parse(
-            response.choices[0].message.function_call.arguments
+          response.choices[0].message.function_call.arguments
         );
 
-        if(response.choices[0].message.function_call.name == "descriptionFunc") {
-          await setCategoryandPriority({...ticket, ...functionArgs}, setData);
-        };
-        console.log(functionArgs)
-        setData((prev) => {return {...prev, ...functionArgs}})
+        if (
+          response.choices[0].message.function_call.name == "descriptionFunc"
+        ) {
+          setCategoryandPriority({ ...ticket, ...functionArgs }, setData);
+        }
+        console.log(functionArgs);
+        setData((prev) => {
+          return { ...prev, ...functionArgs };
+        });
+        return "Success";
       } else {
-        console.log("no data")
-        return  null
-        
+        console.log("no data");
+        return null;
       }
-
     } catch (error) {
       console.error(error);
+      return;
     }
   };
-  
+
+  await retryAsyncFunc(attemps);
+};
 
 const setCategoryandPriority = async (ticket, setData) => {
-  try {
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-0613",
-      messages: [{
-        role:"system",
-        content:"You are a support engineer for SAP, You have a ticket with description: " + ticket.description + " and subject: " + ticket.subject + ". Complete the ticket immediately by calling the getCategoryandPriority function."
-      }],
-      presence_penalty: -0.3,
-      temperature: 0.2,
-      frequency_penalty: -0.3,
-      functions: [
-        {
-          name: "getCategoryandPriority",
-          description: "Call immediately, deduce category and priority from ticket given",
-          parameters: {
-            type: "object",
-            properties: {
-              category:{
-                type:"string",
-                description:"category of ticket Example category: Warning or Error Message "
-              },
-              priority:{
-                type:"string",
-                description:"priority of ticket: Impact on business operations : Low - no impact, Medium - some impact, High - critical impact"
-              }
-            },
+  const attempt = async () => {
+    try {
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo-0613",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a support engineer for SAP, You have a ticket with description: " +
+              ticket.description +
+              " and subject: " +
+              ticket.subject +
+              ". Complete the ticket immediately by calling the getCategoryandPriority function.",
           },
-          required : ["category", "priority"]
-        }
-      ]
-    });
+        ],
+        presence_penalty: -0.3,
+        temperature: 0.2,
+        frequency_penalty: -0.3,
+        functions: [
+          {
+            name: "getCategoryandPriority",
+            description:
+              "Call immediately, deduce category and priority from ticket given",
+            parameters: {
+              type: "object",
+              properties: {
+                category: {
+                  type: "string",
+                  description:
+                    "category of ticket Example category: Warning or Error Message ",
+                },
+                priority: {
+                  type: "string",
+                  description:
+                    "priority of ticket: Impact on business operations : Low - no impact, Medium - some impact, High - critical impact",
+                },
+              },
+            },
+            required: ["category", "priority"],
+          },
+        ],
+      });
 
-    if (response.choices[0].message.function_call) {
-      const functionArgs = JSON.parse(
+      if (response.choices[0].message.function_call) {
+        const functionArgs = JSON.parse(
           response.choices[0].message.function_call.arguments
-      );
-      console.log(functionArgs)
-      setData((prev) => {return {...prev, ...functionArgs}})
-
-    } else {
-      console.log("no data")
-      return  null
-      
+        );
+        console.log(functionArgs);
+        setData((prev) => {
+          return { ...prev, ...functionArgs };
+        });
+        return "Success";
+      } else {
+        console.log("no data");
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+      return;
     }
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
+  };
+
+  await retryAsyncFunc(attempt);
+};
