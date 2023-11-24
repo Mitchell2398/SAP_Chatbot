f256bd0ff4741b733534633f406cd6c18c7340d1
Jack Mitchell <jackmitchell2398@gmail.com>
2023-11-24 11:56:24 +0000

    Remove Firebase functions

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-1Xu9xBealvnvl9T_2G9Fy5_G4pvrOgk",
+  authDomain: "sap-bot-2e9c8.firebaseapp.com",
+  projectId: "sap-bot-2e9c8",
+  storageBucket: "sap-bot-2e9c8.appspot.com",
+  messagingSenderId: "596323676135",
+  appId: "1:596323676135:web:beda6e66112ca7113f1742"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
